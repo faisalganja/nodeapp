df9bfe09aaf7661fb0718262d6d8b5437609fb13
jl <jl@localhost.localdomain>
2025-02-11 23:37:43 +0000

    fifth commit

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const tracer = require('dd-trace').init({
-  sampling: {
-    sampleRate: 0.1, // Sample 10% of requests
-  },
-});
-
+const tracer = require('dd-trace').init();
 const axios = require('axios');
 
 (async () => {
-  try {
-    const { data: hostname } = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
-    process.env.DD_AGENT_HOST = hostname; // Set Datadog Agent host
-    process.env.DD_TRACE_AGENT_PORT = '8126'; // Set Datadog Agent port
-    console.log(`Datadog Agent configured to use host: ${hostname}`);
-  } catch (error) {
-    console.error('Failed to fetch local IP address:', error.message);
-  }
+  const { data: hostname } = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
+  tracer.setUrl(`http://${hostname}:8126`);
 })();
 
 const express = require('express');
