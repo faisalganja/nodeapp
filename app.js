3b880f0df2ee0d51d4f14c01dead5bd0fb670cc4
Faisal Jamil <155971439+faisalganja@users.noreply.github.com>
2025-02-18 19:42:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const tracer = require('dd-trace').init();
 const axios = require('axios');
 
-(async () => {
- const { data: hostname } = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
-tracer.setUrl(`http://${hostname}:8126`);
-})();
+//(async () => {
+ //const { data: hostname } = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
+//tracer.setUrl(`http://${hostname}:8126`);
+//})();
 
 const express = require('express');
 const app = express();
